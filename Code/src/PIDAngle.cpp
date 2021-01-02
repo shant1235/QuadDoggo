3e94e2b18391c593e6bafc410357b328973f3653
Prashant Mistry <43391228+shant1235@users.noreply.github.com>
2021-01-02 02:59:22 -0500

    Finished PID Lib

--- Code/src/PIDAngle.cpp
+++ Code/src/PIDAngle.cpp
@@ -4,8 +4,6 @@
 #include <Encoder.h>
 #include "PIDAngle.h"
 
-
-
 // #define LIMIT_SWITCHA 14
 // #define LIMIT_SWITCHB A17
 // #define CHANNEL_A 3
@@ -13,30 +11,23 @@
 // #define DIR_A 25
 // #define PWM_A 24
 
-
 double pi = 3.1415;
 double newPosition, motorPower;
-double setLocation = 0;//   avoid using pins with LEDs attached
-boolean routine = true;
+double setLocation = 0; //   avoid using pins with LEDs attached
 
-
-// Best PID : Kp = 500, Ki = 1, Kd = 25 hello
-PIDAngle::PIDAngle(int CH_A, int CH_B, int Dir, int pwm_out,int limitSwitch, double leftLimit)
-  : _myEnc(CH_A,CH_B)
-  , _myPID(&newPosition, &motorPower, &setLocation, 1000, 0, 1, DIRECT) 
+// Best PID : Kp = 500, Ki = 1, Kd = 25 
+PIDAngle::PIDAngle(int CH_A, int CH_B, int Dir, int pwm_out, int limitSwitch, double leftLimit)
+    : _myEnc(CH_A, CH_B), _myPID(&newPosition, &motorPower, &_setLocation, 1000, 0, 1, DIRECT)
 {
-    
 
-    _CH_A = CH_A; 
-    _CH_B = CH_B; 
-    _Dir = Dir;
-    _pwm_out = pwm_out;
-    _leftLimit = leftLimit;
-    _setLocation = setLocation;
-    _limitSwitch = limitSwitch;
-    //Encoder _myEnc(_CH_A, _CH_B);
-    //PID _myPID(&newPosition, &motorPower, &setLocation, 1000, 0, 1, DIRECT);
-   
+  _CH_A = CH_A;
+  _CH_B = CH_B;
+  _Dir = Dir;
+  _pwm_out = pwm_out;
+  _leftLimit = leftLimit;
+  _setLocation = setLocation;
+  _limitSwitch = limitSwitch;
+ 
 
   pinMode(_limitSwitch, INPUT);
   _myPID.SetMode(AUTOMATIC);
@@ -46,64 +37,66 @@ PIDAngle::PIDAngle(int CH_A, int CH_B, int Dir, int pwm_out,int limitSwitch, dou
   pinMode(_Dir, OUTPUT);
   pinMode(_pwm_out, OUTPUT);
   Serial.begin(19200);
-
 }
 
-long oldPosition  = -999;
+long oldPosition = -999;
 
-void PIDAngle::moveTo(double setLocation) {
-
+void PIDAngle::moveTo(double setLocation)
+{
+  _setLocation = setLocation;
   //setLocation = map(val,0,1023,0,2*pi);
-  if(setLocation >= _leftLimit){
-    setLocation = _leftLimit;
+  if (_setLocation >= _leftLimit)
+  {
+    _setLocation = _leftLimit;
   }
   _myPID.Compute();
   double temp = _myEnc.read();
-  newPosition = map(temp,0,4435.14,0,2*pi);
+  newPosition = map(temp, 0, 4435.14, 0, 2 * pi);
   Serial.print(motorPower);
   Serial.print(" ");
-  Serial.print(setLocation);
+  Serial.print(_setLocation);
   Serial.print(" ");
   Serial.print(newPosition);
   Serial.print(" ");
   Serial.println(temp);
 
-   if(newPosition <= setLocation ){
-     _myPID.SetControllerDirection(DIRECT);
+  if (newPosition <= _setLocation)
+  {
+    _myPID.SetControllerDirection(DIRECT);
 
-   turnCounterClockWise(motorPower);
-
-   }else if (newPosition > setLocation){
-          _myPID.SetControllerDirection(REVERSE);
-
-  turnClockWise(motorPower);
-
+    turnCounterClockWise(motorPower);
   }
+  else if (newPosition > _setLocation)
+  {
+    _myPID.SetControllerDirection(REVERSE);
 
+    turnClockWise(motorPower);
+  }
 }
 
-
-
-
-void PIDAngle::turnClockWise(double power) {
+void PIDAngle::turnClockWise(double power)
+{
 
   digitalWrite(_Dir, LOW);
   analogWrite(_pwm_out, power);
 }
-void PIDAngle::turnCounterClockWise(double power) {
+void PIDAngle::turnCounterClockWise(double power)
+{
 
   digitalWrite(_Dir, HIGH);
   analogWrite(_pwm_out, power);
 }
-void PIDAngle::stopRotate() {
+void PIDAngle::stopRotate()
+{
   digitalWrite(_Dir, LOW);
   analogWrite(_pwm_out, 0);
 }
 
+void PIDAngle::findBounds()
+{
 
-void PIDAngle::findBounds() {
-
-  while (digitalRead(_limitSwitch) == LOW) {
+  while (digitalRead(_limitSwitch) == LOW)
+  {
     turnClockWise(55);
   }
   _myEnc.write(0);
--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,19 +1,25 @@
 #include <PID_v1.h>
 #include <Arduino.h>
+#include <PIDAngle.h>
 
 #include <Encoder.h>
 
+PIDAngle myPIDAngle(3, 2, 25, 24, 14, PI*2);
+boolean routine = true;
 
-void setup() {
- 
-
-
+void setup()
+{
 }
 
+void loop()
+{
+  if (routine)
+  {
+    myPIDAngle.findBounds();
+    routine = false;
+  }
+  double val = analogRead(15);
+  double setLocation = map(val, 0, 1023, 0, 2 * PI);
 
-void loop() {
-  
+  myPIDAngle.moveTo(setLocation);
 }
-
-
-
